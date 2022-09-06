501b8c4ae78e1f40f6cd8898b49637c641fe9a7d
Romapsp <surkov.ron@gmail.com>
2022-09-06 18:58:52 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  plugins: [
+    'jest',
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
